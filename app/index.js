8420e68d44cf60592e3bd5e4c92236cda5cb2f42
Srujan Dharma <Srujan.23bcs10107@ms.sst.scaler.com>
2024-03-25 22:57:49 +0530

    updated

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function StartPage() {
   return (
     <View className="flex-1 justify-center items-center">
-      <ActivityIndicator size="large" color="black"/>
+      <ActivityIndicator size="large" color="gray"/>
     </View>
   )
 }
